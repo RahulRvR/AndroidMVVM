7253aca07e21296cce5f3c6c1dac7bf47a0a4ab5
Rahul R Vijay <rahul.rv87@gmail.com>
2019-11-10 11:52:59 -0800

    Updating button states. It is not possible to tap two numbers or two operators in a row.

--- app/src/main/java/com/example/mycalculator/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mycalculator/ui/main/MainFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.children
 import com.example.mycalculator.R
 import com.example.mycalculator.models.CalculatorIdResponse
 import com.example.mycalculator.webservice.RestApi
@@ -16,8 +18,6 @@ import kotlinx.android.synthetic.main.main_fragment.*
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.exceptions.UndeliverableException
 import io.reactivex.plugins.RxJavaPlugins
-
-
 
 
 class MainFragment : Fragment() {
@@ -39,40 +39,85 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        // Could be replaced with DI
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         restApi = RestApi()
-        RxJavaPlugins.setErrorHandler {e -> handleError(e) }
+        RxJavaPlugins.setErrorHandler { e -> handleError(e) }
+        initializeCalculator()
     }
 
     fun handleClickListener(view: View) {
-        setInputText(
-            when (view.id) {
-                R.id.button1 -> R.string.number_1
-                R.id.button2 -> R.string.number_2
-                R.id.button3 -> R.string.number_3
-                R.id.button4 -> R.string.number_4
-                R.id.button5 -> R.string.number_5
-                R.id.button6 -> R.string.number_6
-                R.id.button7 -> R.string.number_7
-                R.id.button8 -> R.string.number_8
-                R.id.button9 -> R.string.number_9
-                R.id.button_plus -> R.string.plus
-                R.id.button_minus-> R.string.minus
-                else -> R.string.number_0
-            }
-        )
+        textOutput.text =
+            viewModel.stringBuilder.append(getString(getTextForButton(view.id))).toString()
+        updateButtonStates(isNumber(view.id))
     }
 
-    private fun setInputText(input: Int) {
-        compositeDisposable.addAll(restApi.getCalculationId()
-            .subscribeOn(Schedulers.io())
-            .map {response -> response.body()}
-            .map { t: CalculatorIdResponse -> t.id }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({calculationId -> handleSuccess(calculationId)
-            }, { throwable -> handleError(throwable) }))
+    private fun getTextForButton(id: Int) = when (id) {
+        R.id.button1 -> R.string.number_1
+        R.id.button2 -> R.string.number_2
+        R.id.button3 -> R.string.number_3
+        R.id.button4 -> R.string.number_4
+        R.id.button5 -> R.string.number_5
+        R.id.button6 -> R.string.number_6
+        R.id.button7 -> R.string.number_7
+        R.id.button8 -> R.string.number_8
+        R.id.button9 -> R.string.number_9
+        R.id.buttonPlus -> R.string.plus
+        R.id.buttonMinus -> R.string.minus
+        else -> R.string.number_0
+    }
 
-        textOutput.text = viewModel.stringBuilder.append(getString(input)).toString()
+    private fun isNumber(id: Int) = when (id) {
+        R.id.button1, R.id.button2, R.id.button3,
+        R.id.button4, R.id.button5, R.id.button6,
+        R.id.button7, R.id.button8, R.id.button9 -> true
+        else -> false
+    }
+
+    private fun updateButtonStates(isNumber: Boolean) {
+
+        row1.children.forEach { view ->
+            if (view is Button) {
+                view.isEnabled = !isNumber
+                view.isClickable = !isNumber
+                if (isNumber) {
+                    view.alpha = 0.2f
+                } else {
+                    view.alpha = 1.0f
+                }
+            }
+        }
+
+        row2.children.forEach { view ->
+            view.isEnabled = !isNumber
+            view.isClickable = !isNumber
+            if (isNumber) {
+                view.alpha = 0.2f
+            } else {
+                view.alpha = 1.0f
+            }
+        }
+
+        row3.children.forEach { view ->
+            view.isEnabled = !isNumber
+            view.isClickable = !isNumber
+            if (isNumber) {
+                view.alpha = 0.2f
+            } else {
+                view.alpha = 1.0f
+            }
+        }
+
+        row4.children.forEach { view ->
+            view.isEnabled = isNumber
+            view.isClickable = isNumber
+            if (!isNumber) {
+                view.alpha = 0.2f
+            } else {
+                view.alpha = 1.0f
+            }
+        }
+
     }
 
     override fun onPause() {
@@ -85,10 +130,25 @@ class MainFragment : Fragment() {
     }
 
     private fun handleError(throwable: Throwable) {
-        when(throwable) {
-            is UndeliverableException -> {}
-            else -> {Toast.makeText(context, throwable.message, Toast.LENGTH_LONG).show()}
+        when (throwable) {
+            is UndeliverableException -> {
+            }
+            else -> {
+                Toast.makeText(context, throwable.message, Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    private fun initializeCalculator() {
+        compositeDisposable.addAll(restApi.getCalculationId()
+            .subscribeOn(Schedulers.io())
+            .map { response -> response.body() }
+            .map { t: CalculatorIdResponse -> t.id }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ calculationId ->
+                handleSuccess(calculationId)
+            }, { throwable -> handleError(throwable) })
+        )
     }
 
 }
